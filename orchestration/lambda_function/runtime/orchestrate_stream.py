7b6c3716133a1f78472c275424caf70ac14caa93
Nathan Woodson <nwoodson@mbp-nwoodson01.fios-router.home>
2022-12-19 15:31:45 -0500

    Update lambda to read stream data and send to CoreEventBus

--- orchestration/lambda_function/runtime/orchestrate_stream.py
+++ orchestration/lambda_function/runtime/orchestrate_stream.py
@@ -1,17 +1,29 @@
 import base64
 import json
+import boto3
+
+client = boto3.client('events')
+
 
 def handler(event, context):
     print(event[0])
 
     for payload in event:
+        stream_data = json.loads(base64.b64decode(payload["data"]).decode("utf-8"))
+        event_bus_event = client.put_events(
+            Entries={
+                'Source': 'ingest-api',
+                'DetailType': 'player',
+                'Detail': stream_data,
+                'EventBusName': 'CoreEventBus'
+            }
+        )
         result = {
             "statusCode": 200,
             "headers": {
                 "content-type": "application/json"
             },
-            "detail": json.loads(base64.b64decode(payload["data"]).decode("utf-8"))
-
+            "body": event_bus_event
         }
 
         print(json.dumps(result))
