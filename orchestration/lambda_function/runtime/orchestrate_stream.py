b0d60c86fefc9b461d04760042acf8d42f684cc4
Nathan Woodson <nwoodson@mbp-nwoodson01.fios-router.home>
2022-12-20 09:56:39 -0500

    Lambda code updates

--- orchestration/lambda_function/runtime/orchestrate_stream.py
+++ orchestration/lambda_function/runtime/orchestrate_stream.py
@@ -1,31 +1,43 @@
-import base64
 import json
 import boto3
+from aws_lambda_powertools.utilities.data_classes import event_source, KinesisStreamEvent
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
 client = boto3.client('events')
 
+logger = Logger(service="OrchestrateStreamAddPlayerEvent")
 
-def handler(event, context):
-    print(event[0])
 
-    for payload in event:
-        stream_data = json.loads(base64.b64decode(payload["data"]).decode("utf-8"))
-        event_bus_event = client.put_events(
-            Entries={
-                'Source': 'ingest-api',
-                'DetailType': 'player',
-                'Detail': stream_data,
-                'EventBusName': 'CoreEventBus'
-            }
-        )
-        result = {
-            "statusCode": 200,
-            "headers": {
-                "content-type": "application/json"
-            },
-            "body": event_bus_event
-        }
+@event_source(data_class=KinesisStreamEvent)
+@logger.inject_lambda_context
+def handler(event: KinesisStreamEvent, context: LambdaContext) -> str:
+    kinesis_record = next(event.records).kinesis
 
-        print(json.dumps(result))
+    # if data was delivered as json
+    data = kinesis_record.data_as_json()
 
-        return result
+    add_player_event = {
+        'Source': 'ingest-api',
+        'DetailType': 'player',
+        'Detail': json.dumps(data),
+        'EventBusName': 'CoreEventBus'
+    }
+
+    logger.info(add_player_event)
+
+    event_bus_event = client.put_events(
+        Entries=[add_player_event]
+    )
+
+    result = {
+        "statusCode": 200,
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": event_bus_event
+    }
+
+    logger.info(result)
+
+    return result
